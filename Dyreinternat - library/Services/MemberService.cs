e96af573ec6ebd9e6d4277c1716943e6b99fc9b3
Mohammad Arash Asefi <moa008@edu.zealand.dk>
2025-05-19 17:58:29 +0200

    memberservice done

--- Dyreinternat - library/Services/MemberService.cs
+++ Dyreinternat - library/Services/MemberService.cs
@@ -1,6 +1,35 @@
-﻿namespace dyreinternat___library.Services
+﻿using dyreinternat___library.Models;
+using dyreinternat___library.Repository;
+
+namespace dyreinternat___library.Services
 {
     public class MemberService
     {
+        private IMemberRepo _memberrepo;
+
+        public MemberService(IMemberRepo memberrepo)
+        {
+            _memberrepo = memberrepo;
+        }
+
+        public void Add(MemberAccount memberAccount )
+        {
+            _memberrepo.Add(memberAccount);
+        }
+
+        public List<MemberAccount> GetAll()
+        {
+            return _memberrepo.GetAll();
+        }
+
+        public void delete(int id)
+        {
+            _memberrepo.Delete(id);
+        }
+
+
+
+
+
     }
 }
