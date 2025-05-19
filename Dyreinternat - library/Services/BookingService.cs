37ac4ff0c01f204bb908d99cceb177a5c5b7a3ee
Mohamad Nour Abo Jamil <moa010@edu.zealand.dk>
2025-05-19 18:27:22 +0200

    booking service & bookingrepo

--- Dyreinternat - library/Services/BookingService.cs
+++ Dyreinternat - library/Services/BookingService.cs
@@ -1,6 +1,33 @@
-﻿namespace dyreinternat___library.Services
+﻿using dyreinternat___library.Models;
+using dyreinternat___library.Repository;
+
+namespace dyreinternat___library.Services
 {
     public class BookingService
+
+
     {
+        private IBookingRepo _bookingrepo;
+
+        public BookingService(IBookingRepo bookingrepoRepo)
+        {
+            _bookingrepo = bookingrepoRepo;
+        }
+
+        public void Add(Booking booking)
+        {
+            _bookingrepo.Add(booking);
+        }
+
+        public List<Booking> GetAll()
+        {
+            return _bookingrepo.GetAll();
+        }
+
+        public void Delete(int id)
+        {
+            _bookingrepo.Delete(id);
+        }
     }
 }
+
