f8f4b5eae2645dd50592b8a5856e5c571a8a02dd
Mohamad Nour Abo Jamil <moa010@edu.zealand.dk>
2025-05-19 18:35:50 +0200

    BlogRepo

--- Dyreinternat - library/Repository/IBlogRepo.cs
+++ Dyreinternat - library/Repository/IBlogRepo.cs
@@ -1,6 +1,16 @@
-﻿namespace dyreinternat___library.Repository
+﻿using dyreinternat___library.Models;
+
+namespace dyreinternat___library.Repository
 {
     public interface IBlogRepo
     {
+
+
+        public List<Blog> GetAll();
+
+        public void Add(Blog blog);
+
+        public void Delete(int id);
+
     }
 }
